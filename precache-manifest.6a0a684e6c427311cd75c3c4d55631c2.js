f3f87083f8a19d2396e5565d43e6a46355ebfd7e
Cem KIRAY <jamcry@hotmail.com>
2020-01-06 14:17:47 +0300

    Updates

---
+++ precache-manifest.6a0a684e6c427311cd75c3c4d55631c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bf308ebcfffa6841629acf9e9a3c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa18d6d7a0b4a563c87b",
+    "url": "/static/css/main.4e75a983.chunk.css"
+  },
+  {
+    "revision": "dc294978f380d79ce816",
+    "url": "/static/js/2.8a2c510e.chunk.js"
+  },
+  {
+    "revision": "5599c98b7af73b31fed6a721c4f935e7",
+    "url": "/static/js/2.8a2c510e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa18d6d7a0b4a563c87b",
+    "url": "/static/js/main.12985c44.chunk.js"
+  },
+  {
+    "revision": "a062aba0458f9a99e86f",
+    "url": "/static/js/runtime-main.35fcc34d.js"
+  }
+]);
